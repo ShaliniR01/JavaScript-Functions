bdf2f2f0be4dc6ec18148b468dfd9f23d5bbecd6
Shalini R <jayanthishalini12@gmail.com>
2024-04-28 21:04:08 +0530

    Modified reduce.js

--- reduce.js
+++ reduce.js
@@ -1,7 +1,7 @@
-function myReduce(numbers, sumOfANumber){
+function myReduce(numbers, sumOfAnArray){
     let sum=0;
     for(let i=0;i<numbers.length;i++){
-        sum = sumOfANumber(sum, numbers[i]);
+        sum = sumOfAnArray(sum, numbers[i]);
     }
     return sum;
 }
